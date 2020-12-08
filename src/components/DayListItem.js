8ba39ec2411a7c909b7e8196484093b4cc091dc4
Paulo Santiago <p.santiago796@gmail.com>
2020-12-07 22:03:38 -0500

    create DayList.js component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,17 +2,18 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from 'classnames/bind';
 
-const spotsRemaining = (spots) => {
-  if (spots === 1) {
-    return `${spots} spot remaining`;
-  } else if (spots > 1) {
-    return `${spots} spots remaining`;
-  } else {
-    return `no spots remaining`;
-  }
-};
 
 export default function DayListItem(props) {
+  const spotsRemaining = (spots) => {
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    } else if (spots > 1) {
+      return `${spots} spots remaining`;
+    } else {
+      return `no spots remaining`;
+    }
+  };
+  
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected" : props.selected,
   });
