b325dccc8456a1ebad0ff53ee0cedf07bbba8636
Paulo Santiago <p.santiago796@gmail.com>
2020-12-09 20:02:44 -0500

    create getAppointmentsForDay helper function and pass tests

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export const getAppointmentsForDay = (state, day) => {
+  let appointmentsList = [];
+  for (const days of state.days) {
+    if (days.name === day) {
+      appointmentsList = days.appointments;
+    }
+  }
+  const appointmentsListDetails = [];
+  appointmentsList.forEach(appointment => {
+    for (const key in state.appointments) {
+      if (appointment === state.appointments[key].id) {
+        appointmentsListDetails.push(state.appointments[key]);
+      }
+    }
+  });
+  return appointmentsListDetails;
+};
