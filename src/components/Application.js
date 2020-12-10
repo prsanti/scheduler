3b22326c06710ab6a0b2936a3ee884723bb4e0d6
Paulo Santiago <p.santiago796@gmail.com>
2020-12-10 16:27:52 -0500

    add getInterview helper to Application and now shows interviewer for appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
-import { getAppointmentsForDay }from "../helpers/selectors.js"
+import { getAppointmentsForDay, getInterview }from "../helpers/selectors.js"
 
 import axios from "axios";
 
@@ -24,21 +24,22 @@ export default function Application(props) {
   ]).then((all) => {
     setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
   });
-  // console.log(state.interviewers);
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
-  // const schedule = dailyAppointments.map((appointment) => {
-  //   const interview = getInterview(state, appointment.interview);
+
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
   
-  //   return (
-  //     <Appointment
-  //       key={appointment.id}
-  //       id={appointment.id}
-  //       time={appointment.time}
-  //       interview={interview}
-  //     />
-  //   );
-  // });
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -63,9 +64,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => {
-          return <Appointment key={appointment.id} {...appointment} />
-        })}
+        {schedule}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
