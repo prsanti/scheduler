00d0a1fd883a281186e16d9571b56069b9cab88a
Paulo Santiago <p.santiago796@gmail.com>
2020-12-11 14:39:05 -0500

    implement useVisualMode hook for Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,25 +1,40 @@
 import React, { Fragment } from 'react'
-// import classNames from 'classnames/bind';
 import "./styles.scss";
+import useVisualMode from "../../hooks/useVisualMode";
 
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+  // console.log(props);
+  // props.interviewers = [];
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return(
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview && (
-        <>
-          <Show {...props.interview}/>
-        </>
+      {mode === EMPTY && (<Empty onAdd={() => {
+        console.log("Clicked onAdd")
+        transition(CREATE);
+        }}
+        />)}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
       )}
-      {!props.interview && (
-        <>
-          <Empty />
-        </>
-      )}
+      {/* temporality setting props.interviewers to [] */}
+      {mode === CREATE && <Form name="" interviewers={[]} interviewer={null} onCancel={back} />}
     </article>
   );
 };
