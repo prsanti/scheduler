124e534e0eff586bc18b82250db0e6cf63548944
Paulo Santiago <p.santiago796@gmail.com>
2020-12-13 13:46:22 -0500

    lint fix

--- src/components/InterviewListItem.js
+++ src/components/InterviewListItem.js
@@ -24,4 +24,4 @@ export default function InterviewerListItem(props) {
       {props.selected && props.name}
     </li>
   );
-};
+}
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import classNames from 'classnames/bind';
 import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewListItem"
 
@@ -27,4 +26,4 @@ export default function InterviewerList(props) {
       <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
-};
+}
