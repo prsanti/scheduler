09b4d4d0cdd9f363681aec5bdf177c60d45fe9b5
Paulo Santiago <p.santiago796@gmail.com>
2020-12-13 15:17:17 -0500

    update spotsRemaining functionality in DayListItem

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,15 +4,18 @@ import classNames from 'classnames/bind';
 
 
 export default function DayListItem(props) {
-  const spotsRemaining = (spots) => {
+
+  const spots = props.spots;
+  const spotsRemaining = () => {
     if (spots === 1) {
       return `${spots} spot remaining`;
     } else if (spots > 1) {
       return `${spots} spots remaining`;
     } else {
-      return `no spots remaining`;
+      return `No spots remaining`;
     }
   };
+  // console.log("test", spots);
   
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected" : props.selected,
@@ -24,7 +27,7 @@ export default function DayListItem(props) {
       className={dayClass}
     >
         <h2 className="text--regular">{props.name}</h2> 
-        <h3 className="text--light">{spotsRemaining(props.spots)}</h3>
+        <h3 className="text--light">{spotsRemaining()}</h3>
     </li>
   );
 }
