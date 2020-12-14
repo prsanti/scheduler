7593ae3a3158ad8d7602f400b30a5bb08efe261b
Paulo Santiago <p.santiago796@gmail.com>
2020-12-14 16:45:59 -0500

    add validation for blank interviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,6 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (interviewer === null) {
+      setError("Pleaes select an interviewer");
+      return;
+    }
   
     props.onSave(name, interviewer);
   }
